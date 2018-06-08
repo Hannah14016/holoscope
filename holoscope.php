9ed18aa9906fe66ecaaad50240231b1964740a6c
haruna.iizuka <dubrovnik302@gmail.com>
2018-06-08 01:18:48 +0000

    first commit

---
+++ holoscope.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>占いの館</title>
+    </head>
+    <body>
+        <header>
+            <h1>占いの館</h1>
+        </header>
+        <wrapper>
+            あなたの星座は？
+                <form action = "fortune.php" method ="get">
+                <input type="text" name="holoscope">
+                <input type="submit" value="占う">
+                </form>
+        </wrapper>
+        <footer></footer>
+    </body>
+</html>
+
+<?php
+
+     if(isset($_GET['holoscope'])) {
+     $holoscope = $_GET['holoscope'];
+     }
+     
+     $rank = mt_rand(1, 12);
+     echo $holoscope . "のあなたの運勢は…" .  $rank . "位です";
+     
+     if($rank == 1) {
+         echo "絶好調";
+     }
+     else {
+         echo "残念。また明日";
+     }
