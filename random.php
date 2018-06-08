73c133fcb2eb9b129c936f3862f835836f4828fe
haruna.iizuka <dubrovnik302@gmail.com>
2018-06-08 01:32:54 +0000

    indent random.php

--- random.php
+++ random.php
@@ -10,24 +10,24 @@ $fortune = array(
     "大凶"
     );
 
-    $count = count($fortune);
-    $random = mt_rand(0,$count - 1);
-    $result = $fortune[$random];
+        $count = count($fortune);
+            $random = mt_rand(0,$count - 1);
+                $result = $fortune[$random];
 
     echo "今日のあなたの運勢は" . $result . "です。" . PHP_EOL;
 
-    if ($result == "大吉") {
-        echo "おめでとう！" . PHP_EOL;
-    }
-    elseif ($result == "凶") {
-        echo "気をつけよう。" . PHP_EOL;
-    }
-    elseif ($result == "大凶") {
-        echo "よく考えて行動しよう。" . PHP_EOL;
-    }
-    else {
-        echo "良いことがあるかも。" . PHP_EOL;
-    }
+        if ($result == "大吉") {
+            echo "おめでとう！" . PHP_EOL;
+        }
+        elseif ($result == "凶") {
+            echo "気をつけよう。" . PHP_EOL;
+        }
+        elseif ($result == "大凶") {
+            echo "よく考えて行動しよう。" . PHP_EOL;
+        }
+        else {
+            echo "良いことがあるかも。" . PHP_EOL;
+        }
     
 $item = array(
     "ハンカチ",
@@ -36,9 +36,9 @@ $item = array(
     "長財布",
     );
 
-    $count = count($item);
-    $i_random = mt_rand(0,$count - 1);
-    $item_result = $item[$i_random];
+        $count = count($item);
+            $i_random = mt_rand(0,$count - 1);
+                $item_result = $item[$i_random];
     
 $color = array(
     "ピンクの",
@@ -49,8 +49,8 @@ $color = array(
     );
 
     $count = count($color);
-    $c_random = mt_rand(0,$count - 1);
-    $color_result = $color[$c_random];    
+        $c_random = mt_rand(0,$count - 1);
+            $color_result = $color[$c_random];    
     
     echo "ラッキーアイテムは" . $color_result . $item_result . PHP_EOL;
     
